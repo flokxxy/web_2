30737c81183f15713f2f135582c3e86bca219974
flokxxy <115942207+flokxxy@users.noreply.github.com>
2024-04-21 21:21:49 +0300

    Update form.php

--- task4/form.php
+++ task4/form.php
@@ -1,16 +1,3 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $fio = $_POST['fio'];
-    $phone = $_POST['phone'];
-    $email = $_POST['email'];
-    $birthdate = $_POST['birthdate'];
-    $gender = $_POST['gender'];
-    $programmingLanguages = $_POST['programming-language'];
-    $bio = $_POST['bio'];
-    // Дополнительная обработка данных формы
-    // Например, сохранение в базу данных или отправка по электронной почте
-}
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -24,58 +11,87 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 <body>
+
+<?php
+if (!empty($messages)) {
+    print('<div id="messages">');
+    // Выводим все сообщения.
+    foreach ($messages as $message) {
+        print($message);
+    }
+    print('</div>');
+}
+?>
+
+
 <div class="form-structor">
     <h2 class="form-title">Форма</h2>
     <form action="index.php" method="POST" class="form_main">
         <div class="form-group">
             <label for="fio">ФИО:</label>
-            <input type="text" class="form-control" id="fio" name="fio" required>
+            <input type="text" id="fio" name="fio" required
+                <?php if ($errors['fio']) {print 'class="error"';} ?> value="<?php print $values['fio']; ?>" placeholder="ФИО" />
+            >
         </div>
         <div class="form-group">
             <label for="phone">Телефон:</label>
-            <input type="tel" class="form-control" id="phone" name="phone" required>
+            <input type="tel" id="phone" name="phone" required
+                <?php if ($errors['phone']) {print 'class="error"';} ?> value="<?php print $values['phone']; ?>" placeholder="+7(___)___-__-__" />
+            >
         </div>
         <div class="form-group">
             <label for="email">E-mail:</label>
-            <input type="email" class="form-control" id="email" name="email" required>
+            <input type="email" class="form-control" id="email" name="email" required
+                <?php if ($errors['email']) {print 'class="error"';} ?> value="<?php print $values['email']; ?>" placeholder="email" />
+            >
         </div>
         <div class="form-group">
             <label for="birthdate">Дата рождения:</label>
-            <input type="date" class="form-control" id="birthdate" name="birthdate" required>
+            <input type="date" class="form-control" id="birthdate" name="birthdate" required
+                <?php if ($errors['birthdate']) {print 'class="error"';} ?> value="<?php print $values['birthdate']; ?>" />
+            >
         </div>
         <div class="form-group">
             <label>Пол:</label>
             <div>
-                <input type="radio" id="male" name="gender" value="male" required>
+                <input type="radio" id="male" name="gender" value="male" required
+                    <?php if ($value['gender']==='male') {print 'checked';} ?>"
+                >
                 <label for="male">Мужской</label>
             </div>
             <div>
-                <input type="radio" id="female" name="gender" value="female" required>
+                <input type="radio" id="female" name="gender" value="female"
+                <?php if ($value['gender']==='female') {print 'checked';} ?>"
+                >
                 <label for="female">Женский</label>
             </div>
         </div>
         <div class="form-group">
             <label for="programming-language">Любимый язык программирования:</label>
             <select class="form-control" id="programming-language" name="programming-language" multiple>
-                <option value="Pascal">Pascal</option>
-                <option value="C">C</option>
-                <option value="C++">C++</option>
-                <option value="JavaScript">JavaScript</option>
-                <option value="PHP">PHP</option>
-                <option value="Python">Python</option>
-                <option value="Java">Java</option>
-                <option value="Haskel">Haskel</option>
-                <option value="Clojure">Clojure</option>
-                <option value="Prolog">Prolog</option>
-                <option value="Scala">Scala</option>
+                <option value="Pascal" <?php if (in_array('Pascal', $values['selections'])) { print 'selected'; } ?>>Pascal</option>
+                <option value="C" <?php if (in_array('C', $values['selections'])) { print 'selected'; } ?>>C</option>
+                <option value="C++" <?php if (in_array('C++', $values['selections'])) { print 'selected'; } ?>>C++</option>
+                <option value="JavaScript" <?php if (in_array('JavaScript', $values['selections'])) { print 'selected'; } ?>>JavaScript</option>
+                <option value="PHthon" <?php if (in_array('PHthon', $values['selections'])) { print 'selected'; } ?>>Python</option>
+                <option value="JavP" <?php if (in_array('JavP', $values['selections'])) { print 'selected'; } ?>>PHP</option>
+                <option value="Pya" <?php if (in_array('Pya', $values['selections'])) { print 'selected'; } ?>>Java</option>
+                <option value="Haskel" <?php if (in_array('Haskel', $values['selections'])) { print 'selected'; } ?>>Haskel</option>
+                <option value="Clrolog" <?php if (in_array('Clrolog', $values['selections'])) { print 'selected'; } ?>>Prolog</option>
+                <option value="Sojure" <?php if (in_array('Sojure', $values['selections'])) { print 'selected'; } ?>>Clojure</option>
+                <option value="Pcala" <?php if (in_array('Pcala', $values['selections'])) { print 'selected'; } ?>>Scala</option>
             </select>
         </div>
         <div class="form-group">
             <label for="bio">Биография:</label>
-            <textarea class="form-control" id="bio" name="bio" rows="4" required></textarea>
+            <textarea class="form-control" id="bio" name="bio" rows="4" required>
+            <?php if ($errors['bio']) {print 'class="error"';} ?> value="<?php print $values['bio']; ?>"
+                </textarea>
         </div>
         <div class="form-group form-check">
-            <input type="checkbox" class="form-check-input" id="contract" name="contract" required>
+            <input type="checkbox" class="form-check-input" id="contract" name="contract" required
+                <?php if ($errors['contract']) {print 'class="error"';} ?> value="" />
+            >
             <label class="form-check-label" for="contract">С контрактом ознакомлен(а)</label>
         </div>
         <button type="submit" class="btn btn-primary">Сохранить</button>
