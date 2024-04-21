ed68632aec6647a981c3cb1edb93cf12dd181b1a
flokxxy <115942207+flokxxy@users.noreply.github.com>
2024-04-21 21:35:26 +0300

    Update index.php

--- task4/index.php
+++ task4/index.php
@@ -1,6 +1,17 @@
 <?php
 session_start();
+
+
+header('Content-Type: text/html; charset=UTF-8');
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (!empty($_GET['save'])) {
+        print('Спасибо, результаты сохранены.');
+    }
+    include('form.php');
+    exit();
+}
 include('../impotent.php');
+
 $servername = "localhost";
 $username = username;
 $password = password;
