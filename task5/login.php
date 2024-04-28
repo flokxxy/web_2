55f3b6ee7faacd612164e74d05230b764f24e296
flokxxy <115942207+flokxxy@users.noreply.github.com>
2024-04-28 18:03:59 +0300

    Update login.php

--- task5/login.php
+++ task5/login.php
@@ -19,7 +19,7 @@ $options = [
 ];
 
 try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
+    $pdo = new PDO($dbname, $username, $password, $options);
 } catch (\PDOException $e) {
     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
