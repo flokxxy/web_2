e250ca982d6fd1c206a87deb8d8a69a7ea3b3dfd
flokxxy <115942207+flokxxy@users.noreply.github.com>
2024-04-28 23:44:42 +0300

    Update index.php

--- task5/index.php
+++ task5/index.php
@@ -98,8 +98,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         printf('Вход с логином %s, uid %d', $_SESSION['login'], $_SESSION['uid']);
     }
 
-    $messages[] = sprintf('<a href ="login.php?enter=1> Enter (выход) </a>');
-
+    $messages[] = '<a href ="login.php?enter=1"> Enter (выход) </a>';
 
 
     include('form.php');
