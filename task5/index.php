04322fa168e01b966e5b46266642a7a7c70a60af
flokxxy <115942207+flokxxy@users.noreply.github.com>
2024-04-28 21:02:18 +0300

    Update index.php

--- task5/index.php
+++ task5/index.php
@@ -11,6 +11,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         
         $messages[] = 'Спасибо, результаты сохранены.';
         $messages[] = !empty($_COOKIE['pass']);
+         $messages[] = sprintf('Вы можете <a href="login.php">войти</a> с логином <strong>%s</strong>
+        и паролем <strong>%s</strong> для изменения данных.',
+                strip_tags($_COOKIE['login']),
+                strip_tags($_COOKIE['pass']));
 
 
         if (!empty($_COOKIE['pass'])) {
