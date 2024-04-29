3a3680d9eaf730a2c4434cd7333ef3e087e53bd9
flokxxy <115942207+flokxxy@users.noreply.github.com>
2024-04-29 18:47:18 +0300

    Update config.php

--- SUMMER_PRACTIS/config.php
+++ SUMMER_PRACTIS/config.php
@@ -1,13 +1,13 @@
 <?php
 
-include('../impotent.php');
+include('../../impotent.php');
 $servername = "localhost";
 $username = username;
 $password = password;
 $dbname = username;
 
 try {
-    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Не могу подключиться к базе данных: " . $e->getMessage());
