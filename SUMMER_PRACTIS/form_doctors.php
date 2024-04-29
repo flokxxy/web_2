598f1da9d659495eb634c7b25dc91adae4e27900
flokxxy <115942207+flokxxy@users.noreply.github.com>
2024-04-29 18:08:47 +0300

    Update form_doctors.php

--- SUMMER_PRACTIS/form_doctors.php
+++ SUMMER_PRACTIS/form_doctors.php
@@ -1,12 +1,26 @@
 <?php
-include 'config.php';
+include ('config.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
     $fullName = $_POST['fullName'];
     $specialty = $_POST['specialty'];
     $fee = $_POST['fee'];
     $commission = $_POST['commission'];
 
+    include('../impotent.php');
+    $servername = "localhost";
+    $username = username;
+    $password = password;
+    $dbname = username;
+
+    try {
+        $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die("Не могу подключиться к базе данных: " . $e->getMessage());
+    }
+    
     $sql = "INSERT INTO Doctors (FullName, Specialty, ConsultationFee, Commission) VALUES (?, ?, ?, ?)";
     $stmt = $pdo->prepare($sql);
 
