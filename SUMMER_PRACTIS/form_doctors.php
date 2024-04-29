4cec9fb8b24e3237439cfe3fb2a2f9376c11ccc2
flokxxy <115942207+flokxxy@users.noreply.github.com>
2024-04-29 18:45:04 +0300

    Update form_doctors.php

--- SUMMER_PRACTIS/form_doctors.php
+++ SUMMER_PRACTIS/form_doctors.php
@@ -35,27 +35,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } catch (PDOException $e) {
         die("Ошибка при добавлении врача: " . $e->getMessage());
     }
+}else {
+
+    header("Location: form_doctors.html");
+    exit;
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="UTF-8">
-    <title>task 5</title>
-</head>
-<body>
-<form action="/submit_doctor" method="post">
-    <label for="fullName">ФИО врача:</label>
-    <input type="text" id="fullName" name="fullName" required>
-    <label for="specialty">Специальность:</label>
-    <input type="text" id="specialty" name="specialty" required>
-    <label for="fee">Стоимость приема:</label>
-    <input type="number" id="fee" name="fee" required step="0.01">
-    <label for="commission">Процент отчисления:</label>
-    <input type="number" id="commission" name="commission" required step="0.01">
-    <button type="submit">Добавить врача</button>
-</form>
-</body>
-</html>
