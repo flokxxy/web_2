03babf38c5cfc7b0f2f56b0971bee957b4197e82
flokxxy <115942207+flokxxy@users.noreply.github.com>
2024-04-29 19:57:37 +0300

    Update form_doctors.php

--- SUMMER_PRACTIS/form_doctors.php
+++ SUMMER_PRACTIS/form_doctors.php
@@ -10,7 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $fee = $_POST['fee'];
     $commission = $_POST['commission'];
 
-    include('../../impotent.php');
+    include('../impotent.php');
     $servername = "localhost";
     $username = username;
     $password = password;
