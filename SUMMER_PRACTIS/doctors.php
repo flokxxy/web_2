0b19f81bc7b09b3c46a6af269cfc1149c4f0ee00
flokxxy <115942207+flokxxy@users.noreply.github.com>
2024-04-30 17:38:35 +0300

    Update doctors.php

--- SUMMER_PRACTIS/doctors.php
+++ SUMMER_PRACTIS/doctors.php
@@ -17,17 +17,17 @@
 <form action="form_doctors.php" method="post">
     <label for="fullName">ФИО врача:</label>
     <input type="text" name="fullName"
-    <?php if($errors['fullName']) {print 'class="error"';}?> value="<?php $values['fullName']; ?>" />
+    <?php if($errors['fullName']) {print 'class="error"';}?> value="<?php $values['fullName']; ?>" >
     <label for="specialty">Специальность:</label>
-    <input type="text" id="specialty" name="specialty" 
-    <?php if($errors['specialty']) {print 'class="error"';}?> value="<?php $values['specialty']; ?>"/>
+    <input type="text" id="specialty" name="specialty"
+    <?php if($errors['specialty']) {print 'class="error"';}?> value="<?php $values['specialty']; ?>">
     <label for="fee">Стоимость приема:</label>
-    <input type="number" id="fee" name="fee" 
-    <?php if($errors['fee']) {print 'class="error"';}?> value="<?php $values['fee']; ?>"/>
+    <input type="number" id="fee" name="fee"
+    <?php if($errors['fee']) {print 'class="error"';}?> value="<?php $values['fee']; ?>">
 
     <label for="commission">Процент отчисления:</label>
     <input type="number" id="commission" name="commission"
-    <?php if($errors['commission']) {print 'class="error"';}?> value="<?php $values['commission']; ?> />
+    <?php if($errors['commission']) {print 'class="error"';}?> value="<?php $values['commission']; ?> >
 
     <button type="submit">Добавить врача</button>
 </form>
