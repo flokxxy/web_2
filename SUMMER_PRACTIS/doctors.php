dd026e820dd61f48d883921b82bf3b5e55a3c955
flokxxy <115942207+flokxxy@users.noreply.github.com>
2024-04-30 21:08:10 +0300

    Update doctors.php

--- SUMMER_PRACTIS/doctors.php
+++ SUMMER_PRACTIS/doctors.php
@@ -13,9 +13,22 @@
     <label for="fullName">ФИО врача:</label>
     <input type="text" name="fullName" <?php if (isset($errors['fullName'])) {print 'class="error"';} ?>
     value="<?php if (isset($values['fullName'])) {print $values['fullName'];} ?>" />
+
+
+
     <label for="specialty">Специальность:</label>
-    <input type="text" id="specialty" name="specialty" <?php if (isset($errors['specialty'])) {print 'class="error"';} ?>
+    <!--<input type="text" id="specialty" name="specialty" <?php if (isset($errors['specialty'])) {print 'class="error"';} ?>
     value="<?php if (isset($values['specialty'])) {print $values['specialty'];} ?>" />
+    -->
+    <select id="specialty" name="specialty" <?php if (isset($errors['specialty'])) {echo 'class="error"';} ?>>
+    <option value="">Выберите специальность</option>
+    <option value="терапевт" <?php echo (isset($values['specialty']) && $values['specialty'] == 'терапевт') ? 'selected' : ''; ?>>Терапевт</option>
+    <option value="кардиолог" <?php echo (isset($values['specialty']) && $values['specialty'] == 'кардиолог') ? 'selected' : ''; ?>>Кардиолог</option>
+    <option value="невролог" <?php echo (isset($values['specialty']) && $values['specialty'] == 'невролог') ? 'selected' : ''; ?>>Невролог</option>
+    <option value="хирург" <?php echo (isset($values['specialty']) && $values['specialty'] == 'хирург') ? 'selected' : ''; ?>>Хирург</option>
+    </select>
+
+
     <label for="fee">Стоимость приема:</label>
     <input type="number" id="fee" name="fee" <?php if (isset($errors['fee'])) {print 'class="error"';} ?>
     value="<?php if (isset($values['fee'])) {print $values['fee'];} ?>" />
@@ -24,6 +37,17 @@
     value="<?php if (isset($values['commission'])) {print $values['commission'];} ?>" />
     <button type="submit">Добавить врача</button>
 </form>
+
+
+
+<!--
+<label for="specialty">Специальность:</label>
+<select id="specialty" name="specialty">
+  <option value="терапевт">Терапевт</option>
+  <option value="хирург">Хирург</option>
+  <option value="офтальмолог">Офтальмолог</option>
+   Добавьте другие варианты специальностей по аналогии -->
+</select>
 
 
 <?php
