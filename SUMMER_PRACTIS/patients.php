6e19e6874f6ca367840aec1eb638656dfbf59fcb
flokxxy <115942207+flokxxy@users.noreply.github.com>
2024-04-30 19:07:29 +0300

    Update patients.php

--- SUMMER_PRACTIS/patients.php
+++ SUMMER_PRACTIS/patients.php
@@ -1,4 +1,6 @@
 <?php
+$errors = array();
+$values = array();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,19 +18,19 @@
 <form action="form_patients.php" method="post">
     <label for="lastName">Фамилия:</label>
     <input type="text" id="lastName" name="lastName"
-    <?php if($errors['lastNamee']) {print 'class="error"';}?> value="<?php $values['lastName']; ?>" >
+    <?php if($errors['lastName']) {print 'class="error"';}?> value="<?php print $values['lastName']; ?>" >
     <label for="firstName">Имя:</label>
     <input type="text" id="firstName" name="firstName"
-    <?php if($errors['firstName']) {print 'class="error"';}?> value="<?php $values['firstName']; ?>" >
+    <?php if($errors['firstName']) {print 'class="error"';}?> value="<?php print $values['firstName']; ?>" >
     <label for="middleName">Отчество:</label>
     <input type="text" id="middleName" name="middleName"
-    <?php if($errors['middleName']) {print 'class="error"';}?> value="<?php $values['middleName']; ?>" >
+    <?php if($errors['middleName']) {print 'class="error"';}?> value="<?php print $values['middleName']; ?>" >
     <label for="birthDate">Дата рождения:</label>
     <input type="date" id="birthDate" name="birthDate"
     <?php if ($errors['birthDate']) {print 'class="error"';} ?> value="<?php print $values['birthDate']; ?>">
     <label for="address">Адрес:</label>
     <input type="text" id="address" name="address"
-    <?php if($errors['address']) {print 'class="error"';}?> value="<?php $values['address']; ?>" >
+    <?php if($errors['address']) {print 'class="error"';}?> value="<?php print $values['address']; ?>" >
     <button type="submit">Добавить пациента</button>
 </form>
 </body>
