6ecdf7ea30bf346c0e5e3277e05778eac26f4ca2
flokxxy <115942207+flokxxy@users.noreply.github.com>
2024-05-05 19:47:12 +0300

    Update appointments.php
    add styles

--- SUMMER_PRACTIS/appointments.php
+++ SUMMER_PRACTIS/appointments.php
@@ -4,7 +4,10 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
-    <title>добавление пациента</title>
+    <link rel="stylesheet" href="style_doctors_patients.css" >
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <title>Заявление на прием</title>
 </head>
 <style>
     .error {
@@ -22,29 +25,39 @@ print($errors['birthDate'] . '<br>');
 print($errors['address'] . '<br>');
 */
 ?>
-
+<div class="form-structor">
 <form action="form_appointments.php" method="post">
+    <div class="form-group">
     <label for="lastName">Фамилия:</label>
-    <input type="text" id="lastName" name="lastName" <?php if (($errors['lastName'])) {print 'class="error"';} ?>
+    <input type="text" class="form-control" id="lastName" name="lastName" <?php if (($errors['lastName'])) {print 'class="error"';} ?>
            value="<?php if (isset($values['lastName'])) {print $values['lastName'];} ?>" />
-    <label for="firstName">Имя:</label>
-    <input type="text" id="firstName" name="firstName" <?php if (($errors['firstName'])) {print 'class="error"';} ?>
+    </div>
+    <div class="form-group">
+        <label for="firstName">Имя:</label>
+    <input type="text" class="form-control" id="firstName" name="firstName" <?php if (($errors['firstName'])) {print 'class="error"';} ?>
            value="<?php if (isset($values['firstName'])) {print $values['firstName'];} ?>" />
-    <label for="middleName">Отчество:</label>
-    <input type="text" id="middleName" name="middleName" <?php if (($errors['middleName'])) {print 'class="error"';} ?>
+    </div>
+    <div class="form-group">
+        <label for="middleName">Отчество:</label>
+    <input type="text" class="form-control" id="middleName" name="middleName" <?php if (($errors['middleName'])) {print 'class="error"';} ?>
            value="<?php if (isset($values['middleName'])) {print $values['middleName'];} ?>" />
-    <label for="birthDate">Дата рождения:</label>
-    <input type="date" id="birthDate" name="birthDate" <?php if (($errors['birthDate'])) {print 'class="error"';} ?>
+    </div>
+    <div class="form-group">
+        <label for="birthDate">Дата рождения:</label>
+    <input type="date" class="form-control" id="birthDate" name="birthDate" <?php if (($errors['birthDate'])) {print 'class="error"';} ?>
            value="<?php if (isset($values['birthDate'])) {print $values['birthDate'];} ?>" />
+    </div>
+    <div class="form-group">
     <label for="address">Адрес:</label>
-    <input type="text" id="address" name="address" <?php if (($errors['address'])) {print 'class="error"';} ?>
+    <input type="text" class="form-control" id="address" name="address" <?php if (($errors['address'])) {print 'class="error"';} ?>
            value="<?php if (isset($values['address'])) {print $values['address'];} ?>" />
-
+    </div>
 <br>
        <!-- Опции загружаются из базы данных -->
     <!--
     <label for="doctorId" >Выберите врача:</label>
     -->
+    <div class="form-group">
     <?php
     include('../impotent.php');
     $servername = "localhost";
@@ -80,16 +93,18 @@ print($errors['address'] . '<br>');
     ?>
 
     <label for="date">Дата приема:</label>
-    <input type="datetime-local" id="date" name="date" >
-
+    <input type="datetime-local" class="form-control" id="date" name="date" >
+    </div>
     <!-- это должно выводиться в квитанции
     <label for="paymentAmount">Сумма оплаты:</label>
     <input type="number" id="paymentAmount" name="paymentAmount" required step="0.01">
     -->
-    <button type="submit">Записать на прием</button>
-<a href="quintation.php"><button type="button">Сформировать квитанцию об оплате</button></a>
+    <button type="submit"  class="btnn">Записать на прием</button>
+    <a href="quintation.php"><button type="button">Сформировать квитанцию об оплате</button></a>
+    <button id="generatePdfButton"  class="btnn">Создать PDF</button>
 
 </form>
+</div>
 </body>
 </html>
 
